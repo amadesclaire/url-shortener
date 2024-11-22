e2dc22c9e538ac37a5eb51e3c354e5ad5d56379c
Alexandre Madesclaire <alxm95@gmail.com>
2024-11-22 16:18:42 +0000

    feat: integrate UrlController with routes for URL shortening and statistics in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
 import { Hono } from "@hono/hono";
+import UrlService from "services/urlService.ts";
+import MemoryDB from "db/memoryDB.ts";
+import UrlController from "controllers/urlController.ts";
 
+// Services
+const db = new MemoryDB();
+const urlService = new UrlService(db);
+const urlController = new UrlController(urlService);
+
+// Routes
 const app = new Hono();
+const api = new Hono();
+const v1 = new Hono();
 
-app.get("/", (c) => c.text("Hello Deno!"));
+app.post("/shorten", urlController.create());
+app.get("/shorten/:shortcode", urlController.get());
+app.put("/shorten/:shortcode", urlController.update());
+app.delete("/shorten/:shortcode", urlController.delete());
+app.get("/shorten/:shortcode/stats", urlController.stats());
 
-Deno.serve(app.fetch);
+api.route("/v1", v1);
+app.route("/api", api);
+export default app;
